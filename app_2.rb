68645e5e94f512b37ef3935655c420def7d61292
fthiounn <fthiounn@gmail.com>
2019-10-15 10:44:44 +0200

    v2.0 finished

---
+++ app_2.rb
@@ -0,0 +1,91 @@
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/game'
+require_relative 'lib/player'
+require_relative 'lib/menu'
+
+def perform
+	system 'clear'
+	puts "     _,.
+    ,` -.)
+   ( _/-\\\\-._
+  /,|`--._,-^|            ,
+  \\_| |`-._/||          ,'|
+    |  `-, / |         /  /
+    |     || |        /  /
+     `r-._||/   __   /  /   _______ ________  _     _   ____
+ __,-<_     )`-/  `./  /   / ___  / \\  ___ /  \\|   |/  / __ \\
+'  \\   `---'   \\   /  /    ``  / /   ||   `   ||   || | <__``
+    |           |./  /       _/ /   < |==|    ||   ||  \\__ `\\
+    /           //  /       / /`     ||       ||   ||     `\\ |
+\\_/' \\         |/  /       / /___,,  ||___,   \\ \\_/ / |\\___/ /
+ |    |   _,^-'/  /       /_______/ /______\\   \\___/  \\_____/
+ |    , ``  (\\/  /_
+  \\,.->._    \\X-=/^            Welcome to the game Zeus
+  (  /   `-._//^`              Be the last survivor !
+   `Y-.____(__}				
+    |     {__)
+          ()"
+  #setup : creation of the Human Player
+  puts "Please name your player :"
+  print "> "
+
+  player1 = HumanPlayer.new(gets.chomp)
+
+  #setup : creation of the enemies array
+  enemies = []
+  enemies << Player.new("José")
+  enemies << Player.new("Josianne")
+
+  while (!game_over?(player1,enemies)) do
+  	system 'clear'
+  	player1.show_state
+
+  	#print du battle menu
+  	puts "What to do ?"
+  	puts "a - Find a better weapon ?"
+  	puts "s - Find a health pack ?"
+  	puts "Attack a player in sight :"
+  	for n in 0..enemies.size-1 do
+  		if(enemies[n].life_points >0) then
+	  		puts "#{n} - #{enemies[n].name} who has #{enemies[n].life_points} HP left"
+	  	end
+  	end
+  	user_input = "45"
+
+  	while (user_input != "a") && (user_input !="s") && !user_input.to_i.between?(0,enemies.size-1) do
+  		puts "Please select an action to perform"
+  		print "> "
+  		user_input = gets.chomp
+  	end
+
+  	case user_input
+  	when "a"
+  		player1.search_weapon
+  	when "s"
+  		player1.search_health_pack
+  	else
+  		if (user_input.to_i.between?(0,enemies.size-1)) then
+  			player1.attacks(enemies[user_input.to_i])
+  		end
+  	end
+
+  	#enemies turn
+  	puts "#{player1.name} is under attack !!"
+  	enemies.each {|enemy| enemy.attacks(player1) if enemy.life_points > 0 }
+  	puts "Press enter to continue"
+  	gets
+
+  end
+
+  (player1.life_points > 0)? (puts "Congratulation, you Won !!!") : (puts "You were defeated in battle, what a looser !")
+
+end
+#boolean check if the game is over
+def game_over?(player1, enemies)
+	return true if player1.life_points <= 0
+	return true if enemies.select {|enemy| enemy.life_points > 0}.size == 0
+	return false
+end
+perform
--- lib/player.rb
+++ lib/player.rb
@@ -31,26 +31,91 @@ class Player
 		@name = name
 		@life_points = 10
 	end
+	#¿WHY? displays the player HP
 	def show_state
 		puts "#{@name} has  #{@life_points} HP left"
 	end
+	#¿WHY? apply damage to the player
 	def gets_damage (amount)
 		@life_points -= amount
 		if @life_points < 1 then 
 			puts "Player #{@name} has been killed !"
 		end
 	end
+	#¿WHY? launch an atatck against another player
 	def attacks (player2)
 		puts "Player #{@name} attacks player #{player2.name}"
 		dmg = compute_damage
 		puts "He deals #{dmg} damage points"
 		player2.gets_damage(dmg)
-
+  
 	end
+	#¿WHY? generate randomly the damage dealt
 	def compute_damage
     	return rand(1..6)
   	end
 end
+#¿WHY? Class HumanPlayer inherit from Class Player, this class will differenciate an actual player from
+# monsters
+class HumanPlayer < Player
+	attr_accessor :weapon_level
+	#overloaded constructor
+  	def initialize(name)
+    	super(name)        # appel au constructeur de la classe Player
+    	@life_points = 100
+    	@weapon_level = 1
+    end
+    #¿WHY? displays the player HP
+    def show_state
+    	puts "#{@name} has #{@life_points} HP and a lvl #{@weapon_level} weapon"
+    end
+    #¿WHY? generate randomly the damage dealt with the @weapon_level factor
+    def compute_damage
+        rand(1..6) * @weapon_level
+    end
+    #¿WHY? generates a random weapon and equip it if its better than current one
+    def search_weapon
+    	new_weapon_lvl = rand(1..6)
+    	puts "You found a lvl #{new_weapon_lvl} weapon"
+    	if new_weapon_lvl > @weapon_level then
+    		@weapon_level = new_weapon_lvl
+    		puts "Yeah ! This weapon is better than your current weapon : you equiped the lvl #{@weapon_level} weapon"
+    	else
+    		puts "Your current weapon is stronger :( too bad"
+    	end
+    end
+    #¿WHY? generates a random health pack and uses it
+    def search_health_pack
+    	health_pack = rand(1..6)
+    	if health_pack == 1 then
+    		puts "No health pack found"
+    	elsif health_pack < 6 then
+    		@life_points = life_points + 50
+    		if life_points > 100 then
+    			tmp = 50 - (life_points - 100)
+    			@life_points = 100
+    		end
+    		puts "Yeah, you found a +50 health pack ! You gained +#{tmp} HP"
+    	else
+    		@life_points = life_points + 80
+    		if life_points > 100 then
+    			tmp = 80 - (life_points - 100)
+    			@life_points = 100
+    		end
+    		puts "Yeah, you found a +80 health pack ! You gained +#{tmp} HP"
+    	end
+    end
+end
+
+
+
+
+
+
+
+
+
+
 
 
 
